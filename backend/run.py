c8c9ffc6cb71388268d789395cab033e7daeec8e
Vrund2007 <vrundjotania20@gmail.com>
2026-02-20 19:40:03 +0530

    Final I guess

--- backend/run.py
+++ backend/run.py
@@ -1,22 +1,14 @@
 from backend.authorization import app, db
-from admin import admin_bp
-from provider import provider_bp
-from customer import customer_bp
+from backend.admin import admin_bp
+from backend.provider import provider_bp
+from backend.customer import customer_bp
 
-                              
 app.register_blueprint(admin_bp)
-
-                                 
 app.register_blueprint(provider_bp)
-
-                                 
 app.register_blueprint(customer_bp)
 
-                                                                         
-                                                                                               
 with app.app_context():
     db.create_all()
 
 if __name__ == '__main__':
-                                      
     app.run(debug=True, port=5000)
